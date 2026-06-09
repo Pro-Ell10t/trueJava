b64101ad7f1f5d09f1f3b2f452123010d3022a5e
Usman <sdfg40768@email.com>
2026-06-09 18:12:06 +0500

    Extra variables

--- src/Basics.java
+++ src/Basics.java
@@ -1,18 +1,39 @@
 public class Basics {
     public static void main(String[] args) {
+//       (// used for single line comment)
+//        (/* Block */ used fot Multi line comment)
 //        Newbie lines
+//       (ln after print is stand for new line):
+//        (\n comment stand for new line Start in single Sentence)
+       /* System.out.println("Hello, Friend!");
+        System.out.println("Hello, Friend!");
+        System.out.println("What is your name?");
+        System.out.println("What is your age?");
+        System.out.println("You are not Elliot \nYou are Mastermind"); */
 
-//        System.out.println("Hello, Friend!");
-//        System.out.println("Hello, Friend!");
-//        System.out.println("What is your name?");
-//        System.out.println("What is your age?");
-//        Variables:
+//        Variables Practice
+//        Declaring Variables First
         String name;
-        name = "Usman";
+        name = "usman";
+
         int age;
         age =16;
-        System.out.println("Your name is: "+name);
-        System.out.println("Your age is: "+age);
+
+        double cash;
+        cash = 20.5;
+
+        char currency;
+        currency = '$';
+
+        boolean isRaining;
+        isRaining = false;
+
+//       Using variables values
+        System.out.println("Your name is "+name);
+        System.out.println("You are "+age+" years old");
+        System.out.println("You have "+cash+currency);
+        System.out.println("Raining is= "+isRaining);
+
 
    }
 }
