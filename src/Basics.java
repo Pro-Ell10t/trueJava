08f08de7e2a87fd685368102583b5484f39b6c49
Usman <sdfg40768@email.com>
2026-06-09 16:54:06 +0500

    Basic Variables

---
+++ src/Basics.java
@@ -0,0 +1,18 @@
+public class Basics {
+    public static void main(String[] args) {
+//        Newbie lines
+
+//        System.out.println("Hello, Friend!");
+//        System.out.println("Hello, Friend!");
+//        System.out.println("What is your name?");
+//        System.out.println("What is your age?");
+//        Variables:
+        String name;
+        name = "Usman";
+        int age;
+        age =16;
+        System.out.println("Your name is: "+name);
+        System.out.println("Your age is: "+age);
+
+   }
+}
