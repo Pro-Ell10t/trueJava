1f52219f3af24cd2c404b56512b77582db0ef7fa
Usman <sdfg40768@email.com>
2026-06-10 10:11:44 +0500

    Operators with Scanner methods

--- src/Operators.java
+++ src/Operators.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 public class Operators {
     public static void main(String[] args) {
 //       Addition
 
-        int a = 15;
+      /*  int a = 15;
         int b = 69;
         System.out.println(a+b);
 //        subtraction
@@ -22,5 +24,22 @@ public class Operators {
         int n = 2;
         int m = 10;
         System.out.println(n%m);
+
+//        Multiple Operators
+        int num1 = 55;
+        int num2 = 60;
+        int num3  = 23;
+        double num4 = 98;
+        System.out.println("Answer = "+(num4%num2-num1-num2*num3)); */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Plus(+) 2 amounts");
+        System.out.print("Enter your First amount: ");
+        double amount1 = sc.nextDouble();
+        System.out.print("Enter your Second amount: ");
+        double amount2 = sc.nextDouble();
+        System.out.println("Answer = " +(amount1+amount2));
+
+
+
     }
 }
